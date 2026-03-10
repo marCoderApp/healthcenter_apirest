10cadbf27457a40e61a3d01c64e33dbf6e835016
marCoderApp <farbusfer98@gmail.com>
2026-03-09 23:33:50 -0300

    First commit

---
+++ healthcenterapi/src/main/java/com/marcoder/healthcenterapi/HealthcenterapiApplication.java
@@ -0,0 +1,13 @@
+package com.marcoder.healthcenterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcenterapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcenterapiApplication.class, args);
+	}
+
+}
---
+++ healthcenterapi/src/test/java/com/marcoder/healthcenterapi/HealthcenterapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoder.healthcenterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcenterapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
