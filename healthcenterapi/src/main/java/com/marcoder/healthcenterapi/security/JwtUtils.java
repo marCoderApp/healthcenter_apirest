8d807e0ed8855aa28d401699134080b7a9ea7976
marCoderApp <farbusfer98@gmail.com>
2026-03-18 09:08:12 -0300

    ADD - JwtAuthenticationFilter and JwtUtils classes and their methods

--- healthcenterapi/src/main/java/com/marcoder/healthcenterapi/security/JwtUtils.java
+++ healthcenterapi/src/main/java/com/marcoder/healthcenterapi/security/JwtUtils.java
@@ -1,4 +1,66 @@
 package com.marcoder.healthcenterapi.security;
 
+import java.security.Key;
+import java.util.Date;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtUtils {
+
+    private final String jwtSecret = "AY9a7Fm`_V{E*z_/VcI-P0/KJN6L1k)mp@!k}]E";
+    private final int jwtExpirationMs = 86400000;
+    private final Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+
+    public String generateJwtToken(Authentication authentication) {
+
+        UserDetails userPrincipal = (UserDetails) authentication.getPrincipal();
+
+        return Jwts.builder()
+                .setSubject((userPrincipal.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+    }
+
+    public boolean validateJwtToken(String authToken){
+        try{
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(authToken);
+            return true;
+        }catch(MalformedJwtException e){
+            System.err.println("Invalid JWT token: " + e.getMessage());
+        }catch(ExpiredJwtException e){
+            System.err.println("Expired JWT token: " + e.getMessage());
+        }catch(UnsupportedJwtException e){
+            System.err.println("Unsupported JWT token: " + e.getMessage());
+        }catch(IllegalArgumentException e){
+            System.err.println("JWT claims string is empty: " + e.getMessage());
+        }catch(SecurityException e){
+            System.err.println("JWT signature verification failed: " + e.getMessage());
+        }
+        return false;
+    }
+
+
 }
--- healthcenterapi/src/main/java/com/marcoder/healthcenterapi/security/SecurityConfig.java
+++ healthcenterapi/src/main/java/com/marcoder/healthcenterapi/security/SecurityConfig.java
@@ -1,17 +1,21 @@
 package com.marcoder.healthcenterapi.security;
 
+import org.hibernate.Session;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -19,8 +23,8 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    private UserDetailsService userDetailsService;
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     public SecurityConfig(UserDetailsService userDetailsService, JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.userDetailsService = userDetailsService;
@@ -29,7 +33,7 @@ public class SecurityConfig {
 
     //IT DEFINES HOW TO AUTHENTICATE USERS
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
+    public DaoAuthenticationProvider authenticationProvider() {
 
         //TO SEARCH USERS
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
@@ -55,7 +59,19 @@ public class SecurityConfig {
 
     //IT DEFINES WHO CAN ACCESS TO ENDPOINTS (HTTP SECURITY)
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationProvider authenticationProvider) throws Exception {
+
+        http    .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                ))
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated());
+                http.authenticationProvider(authenticationProvider());
+                http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+
     return null;
     }
 
